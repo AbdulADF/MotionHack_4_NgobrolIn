531a6a9a25993aa718f0c874926c31426b97ce9f
AbdulADF <abduldaffa@gmail.com>
2023-02-25 23:26:42 +0700

    RecyclerView

--- NgobrolInApp/app/src/main/java/com/abduladf/ngobrolin/ui/dashboard/CreatorItemAdapter.kt
+++ NgobrolInApp/app/src/main/java/com/abduladf/ngobrolin/ui/dashboard/CreatorItemAdapter.kt
@@ -1,5 +1,7 @@
 package com.abduladf.ngobrolin.ui.dashboard
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +11,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.abduladf.ngobrolin.R
 import com.abduladf.ngobrolin.databinding.CreatorItemBinding
 import com.abduladf.ngobrolin.model.Creator
+import kotlinx.coroutines.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.net.URL
 
 class CreatorItemAdapter(private val creatorItems: List<Creator>) : RecyclerView.Adapter<CreatorItemAdapter.ViewHolder>() {
 
@@ -24,7 +30,12 @@ class CreatorItemAdapter(private val creatorItems: List<Creator>) : RecyclerView
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val creator = creatorItems?.get(position)
+        val creator = creatorItems.get(position)
+        val imageUrl = creator.profilePhoto
+        CoroutineScope(Dispatchers.Main).launch {
+            val bitmap = downloadImage(imageUrl)
+            holder.creatorImage.setImageBitmap(bitmap)
+        }
         if (creator != null) {
             holder.creatorName.text = creator.name
             holder.creatorProfession.text = creator.professions[0].name
@@ -35,4 +46,21 @@ class CreatorItemAdapter(private val creatorItems: List<Creator>) : RecyclerView
         return creatorItems?.size ?: 0
     }
 
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.creator_image)
+    }
+
+    private suspend fun downloadImage(imageUrl: String): Bitmap? {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url(imageUrl)
+            .build()
+
+        return withContext(Dispatchers.IO) {
+            val response = client.newCall(request).execute()
+            val inputStream = response.body?.byteStream()
+            BitmapFactory.decodeStream(inputStream)
+        }
+    }
 }
